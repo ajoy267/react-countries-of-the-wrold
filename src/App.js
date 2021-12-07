ab7603ebaa09821010131be383d22e7056c3f409
ajoy267 <andrew.joy12@hotmail.com>
2021-12-07 15:48:59 -0800

    got it all to render with a search bar to filter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState('');
+  const [continent, setContinent] = useState('All');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,10 +15,35 @@ function App() {
     fetchData();
   }, []);
 
+  function countryFilter() {
+    return countries.filter((country) => {
+      return (
+        country.name.includes(query) && (country.continent === continent || continent === 'All')
+      );
+    });
+  }
+
   return (
     <>
       <div className="App">
         <h1>Flags of the World</h1>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+        {countryFilter().map((country) => (
+          <div key={country.id}>
+            <div>
+              {country.name} : {country.continent}
+            </div>
+            <div>
+              <img src={`https://flagcdn.com/84x63/${country.iso2.toLowerCase()}.png`} />
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
