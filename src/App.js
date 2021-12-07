0c4410f6dafb73597e85ca7687e17d44df57a9c3
ajoy267 <andrew.joy12@hotmail.com>
2021-12-07 15:57:08 -0800

    got it to filter by continent using drop down bar

--- src/App.js
+++ src/App.js
@@ -29,21 +29,32 @@ function App() {
         <h1>Flags of the World</h1>
         <input
           type="text"
+          placeholder="Search Here"
           value={query}
           onChange={(e) => {
             setQuery(e.target.value);
           }}
         />
-        {countryFilter().map((country) => (
-          <div key={country.id}>
-            <div>
-              {country.name} : {country.continent}
+        <select value={continent} onChange={(e) => setContinent(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Oceania">Oceania</option>
+          <option value="Europe">Europe</option>
+          <option value="North America">North America</option>
+          <option value="Asia">Asia</option>
+          <option value="Africa">Africa</option>
+          <option value="South America">South America</option>
+          <option value="Antarctica">Antarctica</option>
+        </select>
+        <div className="flags">
+          {countryFilter().map((country) => (
+            <div key={country.id}>
+              <div>{country.name}</div>
+              <div>
+                <img src={`https://flagcdn.com/84x63/${country.iso2.toLowerCase()}.png`} />
+              </div>
             </div>
-            <div>
-              <img src={`https://flagcdn.com/84x63/${country.iso2.toLowerCase()}.png`} />
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
